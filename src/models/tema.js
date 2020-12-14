a27e3dba92ae0f7eb669760088acb8a080715070
Juan Carlos Vallejos Paniagua <jcvallejos@uto.edu.bo>
2020-12-14 19:24:45 -0400

    Proyecto final

--- src/models/tema.js
+++ src/models/tema.js
@@ -53,6 +53,61 @@ module.exports ={
             }
             )
         })
+    },
+    nuevotema(tema, publico, id_usuario){
+        return new Promise( (resolve,reject)=>{
+            conexion.query ("insert into tema (tema, tipo, id_usuario) values ( ?, ?, ? ) ",
+            [tema, publico, id_usuario],
+            (err)=>{
+                if (err) reject(err);
+                else resolve();
+            }
+            )
+        })
+    },
+    borrartema(id_tema){
+        return new Promise( (resolve,reject)=>{
+            conexion.query ("delete from tema where id_tema = ?",
+            [id_tema],
+            (err)=>{
+                if (err) reject(err);
+                else resolve();
+            }
+            )
+        })
+    },
+    modificatema(id_tema, tema, id_usuario, publico){
+        return new Promise( (resolve,reject)=>{
+            conexion.query ("update tema set tema= ?, id_usuario= ?, tipo=? "+
+            " where id_tema=?",
+            [tema, id_usuario, publico, id_tema],
+            (err)=>{
+                if (err) reject(err);
+                else resolve();
+            }
+            )
+        })
+    },
+    listareporte() {
+        return new Promise ( (resolve, reject)=>{
+            conexion.query(
+            "select t.tema, u2.apodo , (select count(*) from comentario c2 where c2.id_tema =t.id_tema ) as comentarios "+
+            "from tema t "+
+            "inner join usuario u2 on t.id_usuario =u2.id_usuario "+
+            "order by -comentarios",
+            (err, temas)=>{
+                if(err) reject(err);
+                else resolve(this.avector(temas))
+            }
+            )
+        })
+    },
+    avector(listaobjetos)
+    {
+        var respuesta = [];
+        listaobjetos.forEach(elemento => {
+            respuesta.push (Object.values(elemento));
+        });
+        return respuesta;
     }
-
 }
--- src/routes/administrador.js
+++ src/routes/administrador.js
@@ -1,8 +1,94 @@
 const express = require('express');
 const router = express.Router();
+const session = require('express-session');
+const mtema = require('../models/tema');
+const mcomentario = require('../models/comentario');
+const PDFDocument = require('./pdfkit-tables');
+const path = require('path');
 
-router.get('/', (req,res) =>{
-    res.send ('Esta dentro del Administrador ');
+function estalogueado(req, res, next) {
+    if (req.user) { next(); } else { res.redirect('/'); }
+};
+
+function esadmin(req, res, next) {
+    if (session.usuario.tipo == 2) next(); else res.redirect('/');
+};
+
+router.get('/', estalogueado, esadmin, (req, res) => {
+    res.render('admin/index');
+});
+
+router.get('/foros', estalogueado, esadmin, (req, res) => {
+    mtema.listatemas()
+        .then(temas => {
+            res.render('admin/foros', { temas });
+        })
+});
+
+router.post('/nuevotema', estalogueado, esadmin, (req, res) => {
+    tema = req.body.tema;
+    publico = req.body.publico == 'on' ? 1 : 0;
+    id_usuario = session.usuario.id_usuario;
+    mtema.nuevotema(tema, publico, id_usuario)
+        .then(res.redirect('/administrador/foros'));
+
+});
+router.get('/borrartema/:id', estalogueado, esadmin, (req, res) => {
+    id_tema = req.params.id;
+    mtema.borrartema(id_tema)
+        .then(res.redirect('/administrador/foros'));
+});
+router.post('/modificatema', estalogueado, esadmin, (req, res) => {
+    tema = req.body.tema2;
+    publico = req.body.publico2 == 'on' ? 1 : 0;
+    id_tema = req.body.idtema2
+    id_usuario = session.usuario.id_usuario;
+    mtema.modificatema(id_tema, tema, id_usuario, publico)
+        .then(res.redirect('/administrador/foros'));
+
+});
+
+router.get('/comentarios/:id', estalogueado, esadmin, (req, res) => {
+    id = req.params.id;
+    id_usuario_session = session.usuario.id_usuario;
+    mtema.nuevavista(id)
+        .then(
+            mtema.listacomentarios(id)
+                .then(comentarios => {
+                    res.render('admin/comentarios', { comentarios, idtema: id, id_usuario_session })
+                })
+        )
+});
+
+router.get('/reporte', estalogueado, esadmin, async (req, res, next) => {
+    mtema.listareporte()
+        .then(temas => {
+            const tabla = {
+                headers: ['Tema', 'Creador', 'Comentarios'],
+                rows: temas
+            };
+            var myDoc = new PDFDocument({ bufferPages: true });
+            let buffers = [];
+            myDoc.on('data', buffers.push.bind(buffers));
+            myDoc.on('end', () => {
+                let pdfData = Buffer.concat(buffers);
+                res.writeHead(200, {
+                    'Content-Length': Buffer.byteLength(pdfData),
+                    'Content-Type': 'application/pdf'
+                })
+                    .end(pdfData);
+            });
+            myDoc.image(path.resolve(__dirname + '/../public/imgs/foro2.png'), 15, 25, { width: 100 })
+                .text('El Foro de la verdad', 125, 35);
+            myDoc.font('Helvetica-Bold')
+                .fontSize(25)
+                .text('Listado de temas mas vistos', 135, 85);
+            myDoc.table(tabla, {
+                prepareHeader: () => myDoc.fontSize(13),
+                prepareRow: (row, i) => myDoc.fontSize(12)
+            });
+            myDoc.end();
+        })
 });
 
 module.exports = router;
